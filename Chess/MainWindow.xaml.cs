56d24dc2db778afd4baecc68778b2b77a740595c
Hallworld <prishedko.ilya@yandex.ru>
2025-04-24 22:20:19 +0300

    Add chess engine lib

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using ChessLib;
 using System.Windows.Shapes;
 
 namespace Chess
