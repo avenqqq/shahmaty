8505cfba93dfbcca9774a34ad796c77772eba16c
Ava110n <RuVMakarov@kpfu.ru>
2025-04-26 12:59:27 +0300

    Base field

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -20,6 +20,19 @@ namespace Chess
         public MainWindow()
         {
             InitializeComponent();
+
+            double cellWidth = Field.Width / 8;
+            double cellHeight = Field.Height / 8;
+            for (int i = 0; i < 64; i++)
+            {
+                var cell = new Rectangle();
+                cell.Width = cellWidth;
+                cell.Height = cellHeight;
+                cell.Fill = new SolidColorBrush(((i / 8 + i % 8) % 2) == 0? Colors.Wheat : Colors.Black);
+                cell.Margin = new Thickness(i % 8 * cellWidth, i / 8 * cellHeight, 0, 0);
+                Field.Children.Add(cell);
+            }
+
         }
     }
 }
